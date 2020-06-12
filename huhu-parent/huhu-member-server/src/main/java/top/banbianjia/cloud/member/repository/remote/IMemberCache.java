17a45f2e7f4d216e1e27df75bf73fc88a39d5758
13650708353 <yesterday581>
2020-06-13 01:23:46 +0800

    测试提交过滤

--- huhu-parent/huhu-member-server/src/main/java/top/banbianjia/cloud/member/repository/remote/IMemberCache.java
+++ huhu-parent/huhu-member-server/src/main/java/top/banbianjia/cloud/member/repository/remote/IMemberCache.java
@@ -14,104 +14,104 @@ import java.util.Map;
 
 @FeignClient(name = "redis-server")
 public interface IMemberCache {
-    /**
-     * 设置进入缓存
-     *
-     * @param keyPrefix 前缀
-     * @param key
-     * @param data
-     * @param <T>
-     */
-    @RequestMapping(value = "/redis/set", method = RequestMethod.GET)
-    <T> void set(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key, @RequestParam("data") T data);
+	/**
+	 * 设置进入缓存
+	 *
+	 * @param keyPrefix 前缀
+	 * @param key
+	 * @param data
+	 * @param <T>
+	 */
+	@RequestMapping(value = "/redis/set", method = RequestMethod.GET)
+	<T> void set(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key, @RequestParam("data") T data);
 
-    /**
-     * 从缓存中获得
-     *
-     * @param keyPrefix 前缀
-     * @param key       关键字
-     * @param clazz     返回的类的类型
-     * @param <T>
-     * @return
-     */
-    @RequestMapping(value = "/redis/get", method = RequestMethod.GET)
-    <T> T get(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key, @RequestParam("clazz") Class<T> clazz);
+	/**
+	 * 从缓存中获得
+	 *
+	 * @param keyPrefix 前缀
+	 * @param key       关键字
+	 * @param clazz     返回的类的类型
+	 * @param <T>
+	 * @return
+	 */
+	@RequestMapping(value = "/redis/get", method = RequestMethod.GET)
+	<T> T get(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key, @RequestParam("clazz") Class<T> clazz);
 
-    /**
-     * 时间轴+1
-     *
-     * @param keyPrefix
-     * @param key
-     * @return
-     */
-    @RequestMapping(value = "/redis/incr", method = RequestMethod.GET)
-    Long incr(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key);
+	/**
+	 * 时间轴+1
+	 *
+	 * @param keyPrefix
+	 * @param key
+	 * @return
+	 */
+	@RequestMapping(value = "/redis/incr", method = RequestMethod.GET)
+	Long incr(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key);
 
-    /**
-     * 时间轴-1
-     *
-     * @param keyPrefix
-     * @param key
-     * @return
-     */
-    @RequestMapping(value = "/redis/decr", method = RequestMethod.GET)
-    Long decr(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key);
+	/**
+	 * 时间轴-1
+	 *
+	 * @param keyPrefix
+	 * @param key
+	 * @return
+	 */
+	@RequestMapping(value = "/redis/decr", method = RequestMethod.GET)
+	Long decr(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key);
 
-    /**
-     * 查看是否存在
-     *
-     * @param keyPrefix
-     * @param key
-     * @return
-     */
-    @RequestMapping(value = "/redis/exists", method = RequestMethod.GET)
-    boolean exists(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key);
+	/**
+	 * 查看是否存在
+	 *
+	 * @param keyPrefix
+	 * @param key
+	 * @return
+	 */
+	@RequestMapping(value = "/redis/exists", method = RequestMethod.GET)
+	boolean exists(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key);
 
-    /**
-     * 设置到期时间
-     *
-     * @param keyPrefix     前缀
-     * @param key           关键字
-     * @param expireSeconds 时间
-     * @return
-     */
-    @RequestMapping(value = "/redis/expire", method = RequestMethod.GET)
-    Long expire(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key, @RequestParam("expireSeconds") int expireSeconds);
+	/**
+	 * 设置到期时间
+	 *
+	 * @param keyPrefix     前缀
+	 * @param key           关键字
+	 * @param expireSeconds 时间
+	 * @return
+	 */
+	@RequestMapping(value = "/redis/expire", method = RequestMethod.GET)
+	Long expire(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key, @RequestParam("expireSeconds") int expireSeconds);
 
-    /**
-     * 设置hash
-     *
-     * @param keyPrefix 前缀
-     * @param key       关键字
-     * @param field     字段
-     * @param data      对象
-     * @param <T>
-     */
-    @RequestMapping(value = "/redis/hset", method = RequestMethod.GET)
-    <T> void hset(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key, @RequestParam("field") String field, @RequestParam("data") T data);
+	/**
+	 * 设置hash
+	 *
+	 * @param keyPrefix 前缀
+	 * @param key       关键字
+	 * @param field     字段
+	 * @param data      对象
+	 * @param <T>
+	 */
+	@RequestMapping(value = "/redis/hset", method = RequestMethod.GET)
+	<T> void hset(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key, @RequestParam("field") String field, @RequestParam("data") T data);
 
-    /**
-     * 获得hash
-     *
-     * @param keyPrefix
-     * @param key
-     * @param field
-     * @param clazz
-     * @param <T>
-     * @return
-     */
-    @RequestMapping(value = "/redis/hget", method = RequestMethod.GET)
-    <T> T hget(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key, @RequestParam("field") String field, @RequestParam("clazz") Class<T> clazz);
+	/**
+	 * 获得hash
+	 *
+	 * @param keyPrefix
+	 * @param key
+	 * @param field
+	 * @param clazz
+	 * @param <T>
+	 * @return
+	 */
+	@RequestMapping(value = "/redis/hget", method = RequestMethod.GET)
+	<T> T hget(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key, @RequestParam("field") String field, @RequestParam("clazz") Class<T> clazz);
 
-    /**
-     * 获得hash中的全部
-     *
-     * @param keyPrefix 前缀
-     * @param key       关键字
-     * @param clazz     类
-     * @param <T>
-     * @return
-     */
-    @RequestMapping(value = "/redis/hgetAll", method = RequestMethod.GET)
-    <T> Map<String, T> hgetAll(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key, @RequestParam("clazz") Class<T> clazz);
+	/**
+	 * 获得hash中的全部
+	 *
+	 * @param keyPrefix 前缀
+	 * @param key       关键字
+	 * @param clazz     类
+	 * @param <T>
+	 * @return
+	 */
+	@RequestMapping(value = "/redis/hgetAll", method = RequestMethod.GET)
+	<T> Map<String, T> hgetAll(@RequestParam("keyPrefix") KeyPrefix keyPrefix, @RequestParam("key") String key, @RequestParam("clazz") Class<T> clazz);
 }
